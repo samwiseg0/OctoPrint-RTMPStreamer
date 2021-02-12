ee44fd8db3b151300aec3002871ad9f72791d17c
samwiseg0 <2241731+samwiseg0@users.noreply.github.com>
2021-02-12 01:46:02 -0500

    Really fix the formatting this time

--- octoprint_rtmpstreamer/__init__.py
+++ octoprint_rtmpstreamer/__init__.py
@@ -98,15 +98,15 @@ class rtmpstreamer(octoprint.plugin.StartupPlugin,
                 framerate = str(self._settings.get(["stream_framerate"]))
                 rtmp_stream_url = str(self._settings.get(["stream_url"]))
                 # Define the FFMPEG command to run
-                ffmpeg_cmd = '{} -re -f mjpeg -framerate {} -i {} -ar 44100 -ac 2 -acodec pcm_s16le -f s16le -ac 2 -i ' \
-                             '/dev/zero -acodec aac -ab 128k -strict experimental -s {} -vcodec h264 -pix_fmt yuv420p ' \
-                             '-g 10 -vb 700k -framerate {} -f flv -filter:v {} {rtmp_stream_url}'.format(FFMPEG,
-                                                                                                         framerate,
-                                                                                                         webcam_stream,
-                                                                                                         stream_resolution,
-                                                                                                         framerate,
-                                                                                                         filters,
-                                                                                                         rtmp_stream_url)
+                ffmpeg_cmd = '{FFMPEG} -re -f mjpeg -framerate {framerate} -i {webcam_stream} -ar 44100 -ac 2 -acodec ' \
+                             'pcm_s16le -f s16le -ac 2 -i /dev/zero -acodec aac -ab 128k -strict experimental -s ' \
+                             '{stream_resolution} -vcodec h264 -pix_fmt yuv420p -g 10 -vb 700k -framerate {framerate} ' \
+                             '-f flv -filter:v {filters} {rtmp_stream_url}'.format(FFMPEG,
+                                                                                   webcam_stream,
+                                                                                   stream_resolution,
+                                                                                   framerate,
+                                                                                   filters,
+                                                                                   rtmp_stream_url)
                 # Start the ffmpeg subprocess
                 self.ffmpeg = subprocess.Popen(ffmpeg_cmd.split(' '), stdin=subprocess.PIPE,
                                                         stdout=subprocess.PIPE, stderr=FNULL)
